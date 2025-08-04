42227a725fb62d304cc62d3d90d42e97dd021a91
marianaeslan <mariana.eslan@gmail.com>
2025-08-04 18:17:05 -0300

    feat: add basic Express server and contacts route
    - Create Express server entry point (index.js)
    - Add routes file with /contacts GET endpoint
    - Implement ContactController with index method
    - Configure nodemon and eslint in package.json

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,24 @@
+class ContactController {
+  index(request, response) {
+    response.send('Send from Contact Controller');
+  }
+
+  show() {
+
+  }
+
+  store() {
+
+  }
+
+  update() {
+
+  }
+
+  delete() {
+
+  }
+}
+
+// Singleton
+module.exports = new ContactController();
