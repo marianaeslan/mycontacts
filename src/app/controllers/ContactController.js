ffdecd8d7f9192cd68530b7740ee0e2c0a1e7ee7
marianaeslan <mariana.eslan@gmail.com>
2025-08-07 10:32:07 -0300

    feat: implement route to update contact

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -30,7 +30,7 @@ class ContactController {
     if (contactExists) {
       return response
         .status(400)
-        .json({ error: 'This email is already been taken' });
+        .json({ error: 'This email is already in use' });
     }
 
     const contact = await ContactsRepository.create({
@@ -43,7 +43,37 @@ class ContactController {
     response.json(contact);
   }
 
-  update() {}
+  async update(request, response) {
+    const { id } = request.params;
+    const { name, email, phone, category_id } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response
+        .status(400)
+        .json({ error: 'This email is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    response.json(contact);
+  }
 
   async delete(request, response) {
     const { id } = request.params;
@@ -58,5 +88,4 @@ class ContactController {
   }
 }
 
-// Singleton
 module.exports = new ContactController();
