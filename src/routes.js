a65ea63e90afd89745d01ea94f9589258b5d39de
marianaeslan <mariana.eslan@gmail.com>
2025-08-13 10:28:48 -0300

    feat: add CategoryController and CategoriesRepository with database integration

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 
 const ContactController = require('./app/controllers/ContactController.js');
 
+const CategoryController = require('./app/controllers/CategoryController.js');
+
 const router = Router();
 
 router.get('/contacts', ContactController.index);
@@ -9,5 +11,8 @@ router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts/', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
+
+router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
 
 module.exports = router;
